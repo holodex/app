7ace09c6d9f971ef12c27dce252165d9f89ced95
Michael Williams <dinosaur@riseup.net>
2015-07-06 21:15:49 +1200

    get data from yml, to be converted

---
+++ src/scripts/migrate-yml-data-to-csv.js
@@ -0,0 +1,65 @@
+var FsDb = require('fs-db')
+var yaml = require('js-yaml')
+var pull = require('pull-stream')
+var clone = require('lodash.clone')
+var Model = require('base/model')
+
+var db = FsDb({
+  location: process.cwd() + "/data",
+  idAttribute: '@id',
+  codec: {
+    type: 'yml',
+    encode: function (obj) {
+      return yaml.safeDump(obj)
+    },
+    decode: function (str) {
+      return yaml.safeLoad(str)
+    }
+  }
+})
+
+var graph
+var latestCommit
+
+pullGraph(function (err, graph) {
+  if (err) { throw err; }
+  console.log(graph)
+})
+
+function pullGraph (cb) {
+  pull(
+    db.createReadStream(),
+    pull.map(function (item) {
+      var attrs = clone(item)
+
+      // rename idAttribute to '@id'
+      attrs['@id'] = attrs.id
+      delete attrs.id
+
+      // TODO find more general solution to embedded objects
+      //
+      // if embedded role, reference parent relationship
+      var embeddedRoleRe = /(.*relationships\/[^\/#]+)/
+      var embeddedRoleMatch = attrs['@id'].match(embeddedRoleRe)
+      if (embeddedRoleMatch) {
+        attrs.relationship = embeddedRoleMatch[1]
+      }
+
+      return attrs
+    }),
+    pull.collect(function (err, arr) {
+      if (err) { return cb(err) }
+
+      var pulledGraph = arr
+      .filter(function (item) {
+        // HACK forget about @mixmix's helper objects
+        return !(Object.keys(item).length === 2 && item.agent)
+      })
+      //.map(function (item) {
+      //  return Model.new(item)
+      //})
+
+      cb(null, pulledGraph)
+    })
+  )
+}
