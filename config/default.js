2f492443901fdd48868574c29f3a61f712dd3698
Michael Williams <dinosaur@riseup.net>
2015-07-13 21:09:30 +1200

    move dev-specific config into config/development.js

--- config/default.js
+++ config/default.js
@@ -1,5 +1,3 @@
-var fs = require('fs')
-
 var config = {
   url: {
     protocol: 'http',
@@ -8,20 +6,7 @@ var config = {
   },
   data: {
     db: 'yml',
-    localDir: __dirname + '/../data',
-    email: "31338047380-lndnfeihifv0ibtiic8aa36a9ea2h7m5@developer.gserviceaccount.com",
-    key: fs.readFileSync(__dirname + "/sheet-key.pem"),
-    scopes: ["https://spreadsheets.google.com/feeds"],
-    spreadsheet: "1977Tds2k0c05LcRc3fu8JuF-oZMuDWp4pL8T70UY_5k",
-    worksheets: {
-      groups: '1',
-      people: '2',
-      roles: '3',
-      roleTypes: '7',
-      relationshipTypes: '6',
-      linkTypes: '8',
-      relationships: '4'
-    }
+    localDir: __dirname + '/../data'
   },
   api: {
     url: {
