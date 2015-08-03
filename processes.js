c31cae8a11e042381261a50eb7da91773855321d
Michael Williams <dinosaur@riseup.net>
2015-08-04 00:26:33 +1200

    npm start defaults to production

--- processes.js
+++ processes.js
@@ -1,10 +1,15 @@
+var defined = require('defined')
+
+var env = process.env
+var nodeEnv = env.NODE_ENV
+
 module.exports = {
   apps: [{
     name: "holodex",
     script: "src/server.js",
-    watch: process.env.NODE_ENV === 'development',
+    watch: nodeEnv === 'development',
     env: {
-      NODE_ENV: process.env.NODE_ENV,
+      NODE_ENV: defined(nodeEnv, 'production'),
       NODE_CONFIG_STRICT_MODE: '' // https://github.com/lorenwest/node-config/wiki/Strict-Mode
     }
   }]
