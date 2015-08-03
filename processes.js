fb24bc59ed77dddc74ce6f108545cf7af9ca4536
Michael Williams <dinosaur@riseup.net>
2015-08-03 21:10:46 +1200

    better bin script

--- processes.js
+++ processes.js
@@ -2,6 +2,10 @@ module.exports = {
   apps: [{
     name: "holodex",
     script: "src/server.js",
-    watch: process.env.NODE_ENV === 'development'
+    watch: process.env.NODE_ENV === 'development',
+    env: {
+      NODE_ENV: 'production',
+      NODE_CONFIG_STRICT_MODE: '' // https://github.com/lorenwest/node-config/wiki/Strict-Mode
+    }
   }]
 }
