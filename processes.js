c821d2c95e2c4c4d70067138dd602d6608139616
Michael Williams <dinosaur@riseup.net>
2015-08-04 00:16:14 +1200

    start works with prod or dev

--- processes.js
+++ processes.js
@@ -4,7 +4,7 @@ module.exports = {
     script: "src/server.js",
     watch: process.env.NODE_ENV === 'development',
     env: {
-      NODE_ENV: 'production',
+      NODE_ENV: process.env.NODE_ENV,
       NODE_CONFIG_STRICT_MODE: '' // https://github.com/lorenwest/node-config/wiki/Strict-Mode
     }
   }]
