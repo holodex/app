2a12e198965ea43815fb257adcaa17d84e69e00c
Mikey <michael.williams@enspiral.com>
2015-08-13 12:52:41 +1200

    use master branch of data in development

--- config/development.js
+++ config/development.js
@@ -2,7 +2,6 @@ module.exports = {
   data: {
     db: 'git',
     remoteUrl: 'git://github.com/holodex/enspiral-data',
-    branch: 'csv',
     localDir: __dirname + '/../data',
     cronSpec: '*/2 * * * *'
   }
