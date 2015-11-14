3b3418a12a116811a870d9205600af1e42dcd33f
simontegg <simon@enspiral.com>
2015-11-14 20:38:17 +1300

    use actors pattern to dispatch aditional actions on state change

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import { createStore, combineReducers, createRedux } from 'redux'
 import Ui from 'ui'
 import holodex from 'reducers'
+import actors from 'actors'
 console.log('holodex', holodex)
 var debug = require('debug')('index')
 
@@ -12,6 +13,22 @@ debug('state', state)
 let store = createStore(holodex, state)
 console.log('store', store)
 debug('store', store.getState())
+
+let acting = false
+store.subscribe(function() {
+  // debug('graph', store.getState().graph)
+
+  if (!acting) {
+    acting = true
+
+    for (let actor of actors) {
+      actor(store.getState(), store.dispatch)
+    }
+
+    acting = false
+  }
+})
+
 
 ReactDOM.render(
   React.createElement(Ui, {
