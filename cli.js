c6648deb94cec47188b4631848aabfc17fd421a3
Michael Williams <michael.williams@enspiral.com>
2016-07-27 23:25:34 +1200

    reduce minified + gzipped bundle size from 234.7 kB to 67 kB

--- cli.js
+++ cli.js
@@ -5,6 +5,9 @@ const vas = require('vas')
 const service = require('dex/service')
 const config = require('./config')
 
+const level = require('level-party')
+config.db = level(config.dbPath)
+
 vas.command(service, config, {
   port: config.port,
   url: config.url,
--- config.js
+++ config.js
@@ -1,14 +1,9 @@
 const { join } = require('path')
 
-var config = {
+const config = {
   port: 5000,
   url: 'ws://localhost:5000',
   dbPath: join(__dirname, 'db')
-}
-
-if (!process.browser) {
-  const level = require('level-party')
-  config.db = level(config.dbPath)
 }
 
 module.exports = config
